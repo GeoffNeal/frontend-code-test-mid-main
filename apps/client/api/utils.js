cdc30ff708d7b808920a641c18c6b6f5fbcd0673
Geoff Neal <geoffrey_neal@hotmail.com>
2023-08-08 20:09:41 +0100

    chore: tidied up api

--- apps/client/api/utils.js
+++ apps/client/api/utils.js
@@ -1,10 +1,10 @@
 export const gqlFetch = async (endpoint, query) => {
-    return await fetch(endpoint, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            Accept: 'application/json',
-        },
-        body: JSON.stringify({ query }),
-    }).then((r) => r.json());
-}
+  return await fetch(endpoint, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+    body: JSON.stringify({ query }),
+  }).then((r) => r.json());
+};
