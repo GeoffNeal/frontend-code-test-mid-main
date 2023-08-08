8dea0bc5d9be063d0c7377fff4eee47a83e7f5bb
Geoff Neal <geoffrey_neal@hotmail.com>
2023-08-08 18:31:08 +0100

    feat: product page receiving data from modified server

---
+++ apps/client/pages/product/[id].jsx
@@ -0,0 +1,54 @@
+export default function Product({ product }) {
+    return (
+        <main>
+            <h1>{product.name}</h1>
+        </main>
+    );
+}
+
+export const getStaticPaths = async () => {
+    const res = await fetch("http://127.0.0.1:3001/graphql", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify({ query: `{
+            allProducts {
+              id
+              name
+            }
+          }` }),
+      })
+        .then(r => r.json());
+
+    const products = res.data.allProducts;
+
+    const paths = products.map((product) => ({
+        params: { id: product.id },
+    }));
+
+    return { paths, fallback: true }
+  }
+
+export const getStaticProps = async () => {
+    const res = await fetch("http://127.0.0.1:3001/graphql", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify({ query: `{
+            Product(id: 1) {
+              id
+              name
+            }
+          }` }),
+      })
+        .then(r => r.json())
+        .then(data => {
+            console.log("data returned:", JSON.stringify(data))
+            return data;
+        })
+    return { props: { product: res.data.Product } }
+  }
