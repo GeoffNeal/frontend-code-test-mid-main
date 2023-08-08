aba6876b76556c3051de406147b3675396069d10
Geoff Neal <geoffrey_neal@hotmail.com>
2023-08-08 17:44:37 +0100

    chore: changes to apps

--- apps/client/pages/index.jsx
+++ apps/client/pages/index.jsx
@@ -3,14 +3,11 @@ export default function Home() {
     <main>
       <div className="home">
         <figure>
-          <img
-            src="https://static.octopuscdn.com/logos/logo.svg"
-            alt="Octopus Energy Logo"
-          />
+          <img src="https://static.octopuscdn.com/logos/logo.svg" alt="Octopus Energy Logo" />
         </figure>
         <h1>Welcome to the Octopus Energy Frontend code test!</h1>
         <p>
-          Get started by visiting the <code>/product</code> URL and editing{" "}
+          Get started by visiting the <code>/product</code> URL and editing{' '}
           <code>client/pages/product.js</code>
         </p>
       </div>
