beab9a18191c28f263db59866c69a5cb58e59a2b
Geoff Neal <geoffrey_neal@hotmail.com>
2023-08-08 17:28:56 +0100

    fix: added back jsx extension for lint-staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   // Lint then format TypeScript and JavaScript files
-  '**/*.(ts|tsx|js|jsx)': function (filenames) {
-    return ['eslint --fix ' + filenames.join(' '), 'prettier --write ' + filenames.join(' ')];
-  },
+  "**/*.(ts|tsx|js|jsx)": (filenames) => [
+    `eslint --fix ${filenames.join(" ")}`,
+    `prettier --write ${filenames.join(" ")}`,
+  ],
+
   // Format MarkDown and JSON
-  '**/*.(md|json)': function (filenames) {
-    return 'prettier --write ' + filenames.join(' ');
-  },
+  "**/*.(md|json)": (filenames) => `prettier --write ${filenames.join(" ")}`,
 };
