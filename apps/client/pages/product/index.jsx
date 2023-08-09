0c153412667b3723323f80fa3b6aa673eadefa76
Geoff Neal <geoffrey_neal@hotmail.com>
2023-08-09 10:10:33 +0100

    feat: products index page

--- apps/client/pages/product/index.jsx
+++ apps/client/pages/product/index.jsx
@@ -11,28 +11,24 @@ const Product = ({ product }) => {
   const { id, img_url } = product;
   return (
     <Link href={`/product/${id}`}>
-      <div className='product-img-container layout-padding-x flex flex-center mb flex flex-col'>
-        <Image
-          width={500}
-          height={500}
-          src={img_url}
-          alt={id}
-          className="rounded-mid"
-        />
-        <div className='layout-padding-x layout-padding-y colour-brand-2'>
+      <div className="product-img-container layout-padding-x flex flex-center mb flex flex-col">
+        <Image width={500} height={500} src={img_url} alt={id} className="rounded-mid" />
+        <div className="layout-padding-x layout-padding-y colour-brand-2">
           <h3>{`Product ${id}`}</h3>
         </div>
       </div>
-    </Link> 
-  )
-}
+    </Link>
+  );
+};
 
 export default function Products({ products }) {
   return (
     <main>
-      <h1 className='layout-padding-x'>Products</h1>
+      <h1 className="layout-padding-x">Products</h1>
 
-      {products.map(product => <Product key={product.id} product={product} />)}
+      {products.map((product) => (
+        <Product key={product.id} product={product} />
+      ))}
     </main>
   );
 }
