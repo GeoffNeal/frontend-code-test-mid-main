4436e6fa632a7c3d231b71dc7e32783f399730e1
Geoff Neal <geoffrey_neal@hotmail.com>
2023-08-09 09:04:01 +0100

    chore: small tidy up

--- apps/client/components/Description.jsx
+++ apps/client/components/Description.jsx
@@ -1,10 +1,10 @@
 const Description = ({ content }) => (
-    <div className="description">
-        <div className="bg-brand-1 layout-padding-x layout-padding-y">
-        <h2>Description</h2>
-        <p>{content}</p>
-        </div>
+  <div className="description">
+    <div className="bg-brand-1 layout-padding-x layout-padding-y">
+      <h2>Description</h2>
+      <p>{content}</p>
     </div>
+  </div>
 );
 
 export default Description;
