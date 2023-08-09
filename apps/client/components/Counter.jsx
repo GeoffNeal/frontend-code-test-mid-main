ccdeda1d70fa889916268ad1d26ec3809f3a76de
Geoff Neal <geoffrey_neal@hotmail.com>
2023-08-09 08:52:38 +0100

    feat: created Counter component

---
+++ apps/client/components/Counter.jsx
@@ -0,0 +1,23 @@
+import { useCounter } from 'react-use';
+import classNames from 'classnames';
+
+// returns a function which calls all the given functions
+const callAll = (...fns) => (...args) => fns.forEach(fn => fn && fn(...args));
+
+const Counter = ({ max, min, onIncrement, onDecrement }) => {
+    const [value, { inc, dec }] = useCounter(1, max, min);
+    const btnClasses = classNames('btn', 'rounded-mid', 'p-full', 'line-height-small', 'bg-brand-2');
+
+    const increment = () => callAll(onIncrement(value + 1), inc(1));
+    const decrement = () => callAll(onDecrement(value - 1), dec(1));
+
+    return (
+        <div>
+            <button onClick={decrement} className={btnClasses} disabled={value === min}>-</button>
+            <span className='text-center inline-block min-width-s count'>{value}</span>
+            <button onClick={increment} className={btnClasses} disabled={value === max}>+</button>
+        </div>
+    )
+}
+
+export default Counter;
