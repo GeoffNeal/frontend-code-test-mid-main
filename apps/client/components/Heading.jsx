dc413bfb1116236c34d24e65f11563cb247eec4a
Geoff Neal <geoffrey_neal@hotmail.com>
2023-08-09 08:48:54 +0100

    feat: created Heading component

--- apps/client/components/Heading.jsx
+++ apps/client/components/Heading.jsx
@@ -1,10 +1,12 @@
 const Heading = ({ title, power, quantity }) => {
-    return (
-        <div className="layout-padding-x">
-            <h1>{title}</h1>
-            <p className="text-colour-muted">{power} {'//'} Packet of {quantity}</p>
-        </div>
-    )
-}
+  return (
+    <div className="layout-padding-x">
+      <h1>{title}</h1>
+      <p className="text-colour-muted">
+        {power} {'//'} Packet of {quantity}
+      </p>
+    </div>
+  );
+};
 
 export default Heading;
