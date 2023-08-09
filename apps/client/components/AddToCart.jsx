860df956386da6341584572a2c5b56c089eec6a8
Geoff Neal <geoffrey_neal@hotmail.com>
2023-08-09 08:53:38 +0100

    feat: created AddToCart component

--- apps/client/components/AddToCart.jsx
+++ apps/client/components/AddToCart.jsx
@@ -1,33 +1,38 @@
 // Components
-import { useState } from "react";
-import Counter from "./Counter";
+import { useState } from 'react';
+import Counter from './Counter';
 
 const AddToCart = ({ price }) => {
-    const splitAt = (index, xs) => [xs.slice(0, index), xs.slice(index)];
-    const formattedPrice = parseFloat(splitAt(-2, `${price}`).join('.'));
+  const splitAt = (index, xs) => [xs.slice(0, index), xs.slice(index)];
+  const formattedPrice = parseFloat(splitAt(-2, `${price}`).join('.'));
 
-    const [adjustedPrice, setAdjustedPrice] = useState(formattedPrice);
+  const [adjustedPrice, setAdjustedPrice] = useState(formattedPrice);
 
-    const handleChange = (val) => {
-        setAdjustedPrice(val * formattedPrice);
-    }
+  const handleChange = (val) => {
+    setAdjustedPrice(val * formattedPrice);
+  };
 
-    const addToCart = () => {
-        // Given there are no mutations defined in the server, I'm assuming
-        // this is purely for aesthetic purposes.
-        console.log('Adding to cart...');
-    }
+  const addToCart = () => {
+    // Given there are no mutations defined in the server, I'm assuming
+    // this is purely for aesthetic purposes.
+    console.log('Adding to cart...');
+  };
 
-    return (
-        <div className="layout-padding-x layout-padding-y">
-            <div className="flex flex-spread mb">
-                <b>£{adjustedPrice.toFixed(2)}</b>
-                <Counter min={1} max={20} onIncrement={handleChange} onDecrement={handleChange} />
-            </div>
-            
-            <button onClick={addToCart} className="btn rounded-mid bg-brand-2 text-colour-dark text-size-large p-full full-width">Add to cart</button>
-        </div>
-    );
-}
+  return (
+    <div className="layout-padding-x layout-padding-y">
+      <div className="flex flex-spread mb">
+        <b>£{adjustedPrice.toFixed(2)}</b>
+        <Counter min={1} max={20} onIncrement={handleChange} onDecrement={handleChange} />
+      </div>
+
+      <button
+        onClick={addToCart}
+        className="btn rounded-mid bg-brand-2 text-colour-dark text-size-large p-full full-width"
+      >
+        Add to cart
+      </button>
+    </div>
+  );
+};
 
 export default AddToCart;
